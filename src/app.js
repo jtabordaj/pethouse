c887bff160bb76592a4bdee680f0b40079cbab83
Hidan12 <marcarfe12@gmail.com>
2023-01-10 23:45:48 -0500

    no subir

--- src/app.js
+++ src/app.js
@@ -6,8 +6,9 @@ const router = require("./routes/routesIndex")
 app.use(express.static("public"));
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
-app.use("/", router)
+
 
 app.listen(3000, ()=>{
     console.log("servidor activo en el puerto 3000");
 });
+app.use("/", router)
--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,13 +1,14 @@
 
 
 const base = require("../database/dataBasePr");
-
+const path = require("path")
 const product = base.product;
 const category = base.category;
+// const users = path.join(__dirname, "../views/users/index.ejs")
 
 const indexController = {
     index: (req, res) =>{
-        res.render("index", {product: product, category: category});
+        res.render("./users/index", {product: product, category: category});
     },
     login: (req, res) =>{
         res.render("login");
