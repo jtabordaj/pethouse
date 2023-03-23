4a21b4a2cd2383f8980b4415658535194facf223
M21ariana <m.montoya2107@gmail.com>
2023-03-22 20:29:50 -0500

    Modelo de la factura

---
+++ database/models/factura.js
@@ -0,0 +1,39 @@
+module.exports = (Sequelize, DataTypes) =>{
+    const alias = "Factura";
+
+    const column = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        
+        id_usuario:{
+            type: DataTypes.INTEGER
+        },
+        fecha:{
+            type: DataTypes.DATE
+        },
+        valor: {
+            type: DataTypes.FLOAT
+        },
+
+    }
+
+    const config = {
+        tableName: 'factura',
+        timestamps: false
+    }
+    const Factura = Sequelize.define(alias, column, config);
+
+    //relaciones 
+    Factura.associate = (models)=>{
+        Factura.belongsTo(models.usuario, {
+            as: "facturacion",
+            foreignKey: "id_usuario"
+        })
+    }
+
+
+    return Factura;
+}
