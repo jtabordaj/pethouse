f7a2fffb95c79f60d5a2dc36cec95de6c8941e08
Hidan12 <marcarfe12@gmail.com>
2023-03-29 20:50:45 -0500

    se modifico la eliminacion del producto

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -68,19 +68,19 @@ const productController = {
         //guardar producto en sql       
         try{
             // se busca id de la categoria seleccionada
-            const categoria = await bd.Categoria.findOne({where: {categoria: req.body.category}});
+            //const categoria = await bd.Categoria.findOne({where: {categoria: req.body.category}});
             
             //se busca id de la marca seleccionada
-            const marca = await bd.Marca.findOne({where: {nombre: req.body.marca}});
+            //const marca = await bd.Marca.findOne({where: {nombre: req.body.marca}});
             
             await bd.Producto.create({
-                id_marca: marca.id,
+                id_marca: req.body.marca,
                 nombre: req.body.nombre_producto,
                 precio: req.body.precio,
                 cantidad_descuento: req.body.descuento,
                 img: req.file.filename,
                 descripcion: req.body.descripcion,
-                id_categoria: categoria.id
+                id_categoria: req.body.category
             })
 
         }catch(error){
@@ -97,71 +97,98 @@ const productController = {
     editProduct: async (req, res) =>{
 
         const idProduct = req.params.idProduct;
-        let producto = {};
+       
         try {
             //se busca el producto por el numero de id
-            producto = bd.Producto.findByPk(idProduct)
+            let producto = await bd.Producto.findByPk(idProduct);
+            
+            if(producto){
+                res.render("./products/produc", {
+                    title:"editProduct",
+                    type:"editar", 
+                    box: producto.dataValues, 
+                    category: await bd.Categoria.findAll(), 
+                    marca: await bd.Marca.findAll(),
+                    actions: "/editProduct/" + idProduct + "?_method=PUT",
+                    session: req.session.user
+                });
+            }else{
+                    res.send("Producto no encontrado")
+                }    
         } catch (error) {
             console.log(error);
         }
         
-        //falta modificar para que la vista reciba el producto bien
-        if(producto){
-            res.render("./products/produc", {
-                title:"editProduct",
-                type:"editar", 
-                box: producto, 
-                category: category.filter( c => c.categoria == "categoria"), 
-                typeOfPets: category.filter( c => c.categoria == "tipo_mascota"),
-                actions: "/editProduct/" + idProduct + "?_method=PUT",
-                session: req.session.user
-            });
-        }else{
-                res.send("Producto no encontrado")
-            }
+        
     },
-    modifyProduct: (req, res) =>{
+    modifyProduct: async (req, res) =>{
         const idProduct = req.params.idProduct;
-        const modify = product.find(m => m.id == idProduct);
-        if (req.body.nombre_producto) modify.nombre_producto = req.body.nombre_producto;
-        if (req.body.tipo_mascota)modify.tipo_mascota = req.body.tipo_mascota;
-        if (req.body.marca)modify.marca = req.body.marca;
-        if (req.body.descripcion) modify.descripcion = req.body.descripcion;
-        if (req.body.categoria) modify.categoria = req.body.categoria;
-        if (req.body.precio) modify.precio = req.body.precio;
-        if (req.body.descuento) modify.descuento = req.body.descuento;
-        if (req.file){
-            fs.unlink(__dirname + '../../../public' + modify.img, (err) => {
-                if (err) throw err;
-                console.log("File deleted!");
+        
+        try {
+            await bd.Producto.update({
+                id_marca: req.body.marca,
+                nombre: req.body.nombre,
+                precio: req.body.precio,
+                cantidad_descuento: req.body.cantidad_descuento,
+                descripcion: req.body.descripcion,
+                id_categoria: req.body.category
+            },
+            {
+                where: { id: idProduct } 
             })
-            modify.img = req.file.filename
+
+            if (req.file) await bd.Producto.update({ img: req.file.filename }, { where: {id: idProduct} })
+
+        } catch (error) {
+            console.log(error);
         }
-        fs.writeFileSync(rutaProduct, JSON.stringify(product, null, 2));
+
+
+        // const modify = product.find(m => m.id == idProduct);
+        // if (req.body.nombre_producto) modify.nombre_producto = req.body.nombre_producto;
+        // if (req.body.tipo_mascota)modify.tipo_mascota = req.body.tipo_mascota;
+        // if (req.body.marca)modify.marca = req.body.marca;
+        // if (req.body.descripcion) modify.descripcion = req.body.descripcion;
+        // if (req.body.categoria) modify.categoria = req.body.categoria;
+        // if (req.body.precio) modify.precio = req.body.precio;
+        // if (req.body.descuento) modify.descuento = req.body.descuento;
+        // if (req.file){
+        //     fs.unlink(__dirname + '../../../public' + modify.img, (err) => {
+        //         if (err) throw err;
+        //         console.log("File deleted!");
+        //     })
+        //     modify.img = req.file.filename
+        // }
+        //fs.writeFileSync(rutaProduct, JSON.stringify(product, null, 2));
         res.redirect("/");
         
     },
-    deleteProduct: (req, res) =>{
+    deleteProduct: async (req, res) =>{
         const idProduct = req.params.idProduct;
-        const producto = product.find( p => p.id == idProduct)
+        
+        const producto = await bd.Producto.findByPk(idProduct);
         
         if(producto){
             res.render("./products/deleteProduct", {
-                pro:producto, 
-                category: category.filter( c => c.categoria == "categoria"), 
-                typeOfPets: category.filter( c => c.categoria == "tipo_mascota"),
-                title:"borrarProduct",
+                product: producto.dataValues, 
+                category: await bd.Categoria.findAll(), 
+                marca: await bd.Marca.findAll(),
+                title:"borrarProducto",
                 session: req.session.user
             });
         }else{
                 res.send("Producto no encontrado")
             }
     },
-    delete: (req, res) =>{
+    delete: async (req, res) =>{
         const idProduct = req.params.idProduct;
-        const prod = product.find(p => p.id == idProduct);
-        prod.borrado = true;
-        fs.writeFileSync(rutaProduct, JSON.stringify(product, null, 2));
+        
+        await bd.Producto.destroy({where: {id: idProduct}})
+        
+        // const prod = product.find(p => p.id == idProduct);
+        // prod.borrado = true;
+        // fs.writeFileSync(rutaProduct, JSON.stringify(product, null, 2));
+        
         res.redirect("/");
     },
     listProduct: (req,res)=>{ 
