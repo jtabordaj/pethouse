6ea886ac48b1fac08cb079febf271bd6a9741e70
M21ariana <m.montoya2107@gmail.com>
2023-04-05 18:34:31 -0500

    Validaciones del Loging y el Register

---
+++ public/js/validacionLogin.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', function() {
+    let form = document.querySelector("form")
+    console.log(form.querySelectorAll("input"));
+
+    let error = [];
+    let email = document.querySelector("#userLogin");
+    let clave = document.querySelector("#passwordLogin");
+
+
+
+email.addEventListener('blur', function(e){
+    console.log(email.value.length)
+    if(email.value.includes('@') && email.value.includes('.') ){
+        email.style.border = '1px solid green'
+    }
+
+    else{
+        email.style.border = '1px solid red'
+        error.push("Ingresar un email válido")
+    }
+});
+
+clave.addEventListener('blur', function(e){
+    console.log(clave.value.length)
+    if(clave.value != ""){
+        clave.style.border = '1px solid green'
+    }
+    else{
+        clave.style.border = '1px solid red'
+        error.push("Ingresar una clave")
+    }
+});
+
+form.addEventListener("submit", (e)=>{
+  
+    if(error.length>0){
+        e.preventDefault();
+    }
+});
+
+});
--- public/js/validacionUsuario.js
+++ public/js/validacionUsuario.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function() {
-    let form = document.querySelector("form")
-    console.log(form.querySelectorAll('input'));
+    let error = [];
+    let form = document.querySelector("form");
+    let imagen = document.querySelector('#img');
     let nombre = form.querySelector('#name');
     let usuario = form.querySelector('#user');
     let email = form.querySelector('#email');
@@ -8,24 +9,50 @@ window.addEventListener('load', function() {
     let clave = form.querySelector('#password');
     let clave2 = form.querySelector('#password2');
 
+    console.log(form.querySelectorAll("input"))
+
     nombre.addEventListener('blur', function(e){
         console.log(nombre.value.length)
-        if(nombre.value.length > 2){
+        if(nombre.value.length >= 2){            
             nombre.style.border = '1px solid green'
         }
         else{
             nombre.style.border = '1px solid red'
-        }
-        
+            error.push("Ingresar un nombre con mínimo dos caracteres")
+        }        
     } )
 
+       
     email.addEventListener('blur', function(e){
         console.log(email.value.length)
         if(email.value.includes('@') && email.value.includes('.') ){
             email.style.border = '1px solid green'
         }
+
         else{
             email.style.border = '1px solid red'
+            error.push("Ingresar un email válido")
+        }
+    })
+
+    clave.addEventListener('blur', function(e){
+        console.log(clave.value.length)
+        if(clave.value.length >= 8){
+            clave.style.border = '1px solid green'
+        }
+        else{
+            clave.style.border = '1px solid red'
+            error.push("Ingresar una clave con mínimo 8 caracteres")
+        }
+    })
+
+    clave2.addEventListener('blur', function(e){
+        console.log(clave2.value.length)
+        if(clave2.value.length >= 8){
+            clave2.style.border = '1px solid green'
+        }
+        else{
+            clave2.style.border = '1px solid red'
         }
     })
 
@@ -34,8 +61,36 @@ window.addEventListener('load', function() {
 
 
 
-
     form.addEventListener("submit", (e)=>{
+        if (imagen.value != "") {
+
+            if (imagen.value.toLowerCase().includes("jpg") || imagen.value.toLowerCase().includes("jpeg") || imagen.value.toLowerCase().includes("png") || imagen.value.toLowerCase().includes("gif") ) {
+                    imagen.style.border = "3px solid green"
+            } 
+            
+            else {
+                imagen.style.border = "3px solid red"
+                error.push("formato de imagen no reconocido")
+            }
+        }  
+
+        else{
+            imagen.style.border = "3px solid red"
+            error.push("formato de imagen no reconocido")
+        };
+
+        if(clave.value != clave2.value){
+            error.push("La contraseña no coincide")
+        }
+
+    
+        if(error.length>0){
+            e.preventDefault();
+        }
+               
+       
+       
+       
     //     let error = [];
     //     e.preventDefault();
     //     let nombre = form.querySelector("#nombre_producto")
