ac1c0a4b6db47570e1bfbcfccb03e5d1e094f324
angela1719 <angelarm783@gmail.com>
2023-04-10 09:10:40 -0500

    validacion de front v2

--- public/js/validacionProducto.js
+++ public/js/validacionProducto.js
@@ -6,7 +6,6 @@ window.addEventListener('load', function() {
     
     form.addEventListener("submit", (e)=>{
         let error = [];
-        e.preventDefault();
         let nombre = form.querySelector("#nombre_producto")
         let edad = form.querySelector("#edad")
         let descripcion = form.querySelector("#descripcion")
@@ -14,29 +13,37 @@ window.addEventListener('load', function() {
         let descuento = form.querySelector("#descuento")
         let imagen = form.querySelector("#imagenProducto")
     
-        
-        if (nombre.value.length < 5 ) {
-            console.log("entro");
+        //validacion del nombre
+        if (nombre.value.length <= 5 ) {
             nombre.style.border = "1px solid red"
             error.push("el nombre debe de contener al menos 5 caracteres")
         } else {
             nombre.style.border = "1px solid green"
         }
+        //validacion de la edad
+        if (edad.value.length < 2 ) {
+            edad.style.border = "1px solid red"
+            error.push("la edad debe de contener al menos 2 caracteres")
+        } else {
+            edad.style.border = "1px solid green"
+        }
 
+        //validacion de precio
+        if (isNaN(precio.value) || precio.value == "") {
+            
+            precio.style.border = "1px solid red"
+            error.push("el precio debe de contener al menos un numero")
+        } else {
+            precio.style.border = "1px solid green"
+        }
 
+        //validacion de la descripcion
         if (descripcion.value.length < 20 ) {
             descripcion.style.border = "1px solid red"
             error.push("La descripcion debe de contener al menos 20 caracteres")
         } else {
             descripcion.style.border = "1px solid green"
         }
-        if (descripcion.value.length < 20 ) {
-            descripcion.style.border = "1px solid red"
-            error.push("La descripcion debe de contener al menos 20 caracteres")
-        } else {
-            descripcion.style.border = "1px solid green"
-        }
-        
         if (imagen.value != "") {
 
             if (imagen.value.toLowerCase().includes("jpg") || imagen.value.toLowerCase().includes("jpeg") || imagen.value.toLowerCase().includes("png") || imagen.value.toLowerCase().includes("gif") ) {
@@ -51,10 +58,17 @@ window.addEventListener('load', function() {
             imagen.style.border = "3px solid red"
             error.push("formato de imagen no reconocido")
         }
+        console.log(form.querySelector(".categoria"));
         
         
         
-        
-    console.log(imagen.value);
+    if(error.length > 0){
+        e.preventDefault();
+        let ulError = document.querySelector(".error")
+        ulError.innerHTML = ""
+        for (let e in error){
+            ulError.innerHTML += `<li> ${error[e]} </li>`
+        }
+    }
     })
 });
--- public/js/validacionUsuario.js
+++ public/js/validacionUsuario.js
@@ -62,6 +62,43 @@ window.addEventListener('load', function() {
 
 
     form.addEventListener("submit", (e)=>{
+
+
+
+        if(nombre.value.length >= 2){            
+            nombre.style.border = '1px solid green'
+        }
+        else{
+            // if(!error.includes("Ingresar un nombre con mínimo dos caracteres")){
+            //     nombre.style.border = '1px solid red'
+                error.push("Ingresar un nombre con mínimo dos caracteres")
+            // }
+        }        
+
+
+        if(email.value.includes('@') && email.value.includes('.') ){
+            email.style.border = '1px solid green'
+        }
+
+        else{
+            // if(!email.includes("Ingresar un email válido")){
+            //     nombre.style.border = '1px solid red'
+                error.push("Ingresar un email válido")
+            // }
+        
+        }
+
+
+
+        if(clave.value.length >= 8){
+            clave.style.border = '1px solid green'
+        }
+        else{
+            clave.style.border = '1px solid red'
+            error.push("Ingresar una clave con mínimo 8 caracteres")
+        }
+
+
         if (imagen.value != "") {
 
             if (imagen.value.toLowerCase().includes("jpg") || imagen.value.toLowerCase().includes("jpeg") || imagen.value.toLowerCase().includes("png") || imagen.value.toLowerCase().includes("gif") ) {
@@ -84,63 +121,15 @@ window.addEventListener('load', function() {
         }
 
     
-        if(error.length>0){
+        if(error.length > 0){
             e.preventDefault();
+            let ulError = document.querySelector(".error")
+            ulError.innerHTML = ""
+            for (let e in error){
+                ulError.innerHTML += `<li> ${error[e]} </li>`
+            }
         }
                
        
-       
-       
-    //     let error = [];
-    //     e.preventDefault();
-    //     let nombre = form.querySelector("#nombre_producto")
-    //     let edad = form.querySelector("#edad")
-    //     let descripcion = form.querySelector("#descripcion")
-    //     let precio = form.querySelector("#precio")
-    //     let descuento = form.querySelector("#descuento")
-    //     let imagen = form.querySelector("#imagenProducto")
-    
-        
-    //     if (nombre.value.length < 5 ) {
-    //         console.log("entro");
-    //         nombre.style.border = "1px solid red"
-    //         error.push("el nombre debe de contener al menos 5 caracteres")
-    //     } else {
-    //         nombre.style.border = "1px solid green"
-    //     }
-
-
-    //     if (descripcion.value.length < 20 ) {
-    //         descripcion.style.border = "1px solid red"
-    //         error.push("La descripcion debe de contener al menos 20 caracteres")
-    //     } else {
-    //         descripcion.style.border = "1px solid green"
-    //     }
-    //     if (descripcion.value.length < 20 ) {
-    //         descripcion.style.border = "1px solid red"
-    //         error.push("La descripcion debe de contener al menos 20 caracteres")
-    //     } else {
-    //         descripcion.style.border = "1px solid green"
-    //     }
-        
-    //     if (imagen.value != "") {
-
-    //         if (imagen.value.toLowerCase().includes("jpg") || imagen.value.toLowerCase().includes("jpeg") || imagen.value.toLowerCase().includes("png") || imagen.value.toLowerCase().includes("gif") ) {
-    //             console.log("entro al switch");
-    //             imagen.style.border = "3px solid green"
-    //         } else {
-    //             imagen.style.border = "3px solid red"
-    //             error.push("formato de imagen no reconocido")
-    //         }
-            
-    //     }else{
-    //         imagen.style.border = "3px solid red"
-    //         error.push("formato de imagen no reconocido")
-    //     }
-        
-        
-        
-        
-    // console.log(imagen.value);
     })
 });
